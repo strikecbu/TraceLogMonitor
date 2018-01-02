0784b037706a395b522dec3ce1e74a69bde9397c
andychen <andy.chen@iisigroup.com>
2018-01-02 17:21:35 +0800

    [System] modify config file and add Costants

---
+++ src/main/java/com/citi/Constants.java
@@ -0,0 +1,26 @@
+package com.citi;
+
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+
+/**
+ * Created by VALLA on 2017/12/28.
+ */
+public interface Constants {
+//  Email config
+    String LOCAL_TEST = "LOCAL_TEST";
+    String EDM_FROM_ADDR = "EDM_FROM_ADDR";
+    String EDM_FROM_PERSON = "EDM_FROM_PERSON";
+    String EDM_HOST = "EDM_HOST";
+    String EDM_USR = "EDM_USR";
+    String EDM_PWD = "EDM_PWD";
+
+//  # Log scan
+//    log path
+    String LOG_FOLDER_PATH = "logFolderPath";
+    String TEMP_FOLDER_PATH = "tempFolderPath";
+    String ISSUE_LOG_FOLDER_PATH = "issueLogFolderPath";
+//    pendingLimit
+    String ALLOW_PENDING_LIMIT = "allowPendingLimit";
+    String EMAIL_SEND_TARGETS = "emailSendTargets";
+
+}
